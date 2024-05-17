d024ac157103cf90a48a32fa3fa76e930bfd8c12
gaurondorian@gmail.com <gaurondorian@gmail.com>
2024-05-18 00:19:34 +0200

    run tests

--- app/server.go
+++ app/server.go
@@ -3,8 +3,6 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"log"
-
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
@@ -15,27 +13,35 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		panic(err)
 	}
 
 	defer l.Close()
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			panic(err)
+		}
+
+		go func() {
+			err := readLoop(conn)
+			if err != nil {
+				panic(err)
+			}
+		}()
 	}
 
+}
+
+func readLoop(conn net.Conn) error {
 	buffer := make([]byte, 1024)
 
-	_, err = conn.Read(buffer)
+	_, err := conn.Read(buffer)
 	if err != nil {
-		fmt.Println("Error reading: ", err.Error())
-		os.Exit(1)
+		return err
 	}
 
 	if bytes.HasPrefix(buffer, []byte("*1\r\n$4\r\nPING\r\n")) {
@@ -46,6 +52,5 @@ func main() {
 		}
 	}
 
-	log.Printf("read command:%s", buffer)
-
+	return nil
 }
