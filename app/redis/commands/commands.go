13170a56e334735fdf25ea394d1a580e40b0d02a
gaurondorian@gmail.com <gaurondorian@gmail.com>
2024-05-27 14:01:47 +0200

    run tests

--- app/redis/commands/commands.go
+++ app/redis/commands/commands.go
@@ -105,11 +105,7 @@ func (_ Get) Receive(conn net.Conn, args [][]byte, server Node) error {
 
 		res, err := utils.ParseFile(path)
 
-		fmt.Printf("%v\n", res)
-		fmt.Println(NewBulkString(res[key]).String())
-		fmt.Println(builder.EncodeAsSimpleString(res[key], BULK_STRING).String())
-
-		_, err = fmt.Fprintf(conn, NewBulkString(res[key]).String())
+		_, err = fmt.Fprintf(conn, builder.EncodeAsSimpleString(res[key], BULK_STRING).String())
 
 		return err
 
